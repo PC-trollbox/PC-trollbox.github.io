5cc539df0ca583ae1a3072c7aa4c3eab35aa2069
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2021-08-20 17:30:21 +0300

    Create announce.js

---
+++ announce.js
@@ -0,0 +1,7 @@
+//Announcing CSS
+document.head.insertAdjacentHTML("beforeend", "<style>.banner { background:black; color: white; text-align: center; height: "+screen.height+"px; width: " + screen.width + "px; }");
+//Announce function
+function createAnnounce(message = "Missing String", title = "Announcement"){
+  var codeID = Math.random().toString().replace(".", "");
+  document.body.insertAdjacentHTML("beforeend", "<div class=\"banner\" id=\"" + codeID + "\"><h1>" + title + " <button onclick=\""+codeID+\".remove()\">X</button></h1><br>" + message + "</div>")
+}
