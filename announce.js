9d44b6a924adec84ef2d530ebe869209991368e2
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2021-08-20 17:37:34 +0300

    Update announce.js

--- announce.js
+++ announce.js
@@ -1,7 +1,7 @@
 //Announcing CSS
-document.head.insertAdjacentHTML("beforeend", "<style>.banner { background:black; color: white; text-align: center; height: "+screen.height+"px; width: " + screen.width + "px; opacity: 50%; top: 0; right: 0; }");
+document.head.insertAdjacentHTML("beforeend", "<style>.banner { background:black; color: white; text-align: center; height: "+screen.height+"px; width: " + screen.width + "px; opacity: 50%; top: 0; right: 0; position: fixed; }");
 //Announce function
 function createAnnounce(message = "Missing String", title = "Announcement"){
   var codeID = Math.random().toString().replace(".", "");
-  document.body.insertAdjacentHTML("beforeend", "<div class=\"banner\" id=\"" + codeID + "\"><h1>" + title + " <button onclick=\""+codeID+".remove()\">X</button></h1><br>" + message + "</div>")
+  document.body.insertAdjacentHTML("beforeend", "<div class=\"banner\" id=\"" + codeID + "\"><h1>" + title + " <button onclick=\"document.getElementByID('"+codeID+"').remove()\">X</button></h1><br>" + message + "</div>")
 }
