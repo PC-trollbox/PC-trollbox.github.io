d5abcd54419567af33241eaf1ed8cef93a51441a
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2021-08-21 17:46:03 +0300

    Update basescript.js

--- basescript.js
+++ basescript.js
@@ -1,7 +1,7 @@
 onload = function() {
     onload = null;
     if (!localStorage.getItem("accepted")) {
-        createAnnounce("This site uses cookies. We don't steal any data from you and will not do this.<br><button onclick=\"this.parentElement.parentElement.remove();localStorage.setItem('accepted','1');location.reload();\">Accept</button><button onclick=\"document.body.innerHTML='User didn\\'t accept the cookie policy. Restart page if you changed your mind.'\">Not accept</button>", "Cookies! Nom nom.<div hidden>");
+        createAnnounce("This site uses cookies. We don't steal any data from you and will not do this.<br><button onclick=\"this.parentElement.parentElement.remove();localStorage.setItem('accepted','1');location.reload();\">Accept</button><button onclick=\"document.body.innerHTML='User didn\\'t accept the cookie policy. Restart page if you changed your mind.'\">Not accept</button>", "Cookies! Nom nom.", true);
     } else {
         createAnnounce("Created cookie notice for newcomers.");
     }
