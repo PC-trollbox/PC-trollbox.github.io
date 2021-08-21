fe6bd7ccfb6d87685dbf32e61507310215f1531e
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2021-08-21 16:55:02 +0300

    Update announce.js

--- announce.js
+++ announce.js
@@ -38,19 +38,31 @@ function createAnnounce(message = "Missing String", title = "Announcement", hide
       }
     },
     refreshDialog: function(){
-      if (hideStore) document.getElementById(codeID).innerHTML = "<h1 style=\"color:white\">" + storeTitle + " <button disabled title=\"Seems like a multichoice, you need to choose any setting to continue.\">x</button></h1><br><label style=\"color:white\">" + storeMessage + "</label>";
-      if (!hideStore) document.getElementById(codeID).innerHTML = "<h1 style=\"color:white\">" + storeTitle + " <button onclick=\"document.getElementById('"+codeID+"').remove()\">x</button></h1><br><label style=\"color:white\">" + storeMessage + "</label>";
-      return "Dialog settings refreshed";
+      if (document.getElementById(codeID)){
+        if (hideStore) document.getElementById(codeID).innerHTML = "<h1 style=\"color:white\">" + storeTitle + " <button disabled title=\"Seems like a multichoice, you need to choose any setting to continue.\">x</button></h1><br><label style=\"color:white\">" + storeMessage + "</label>";
+        if (!hideStore) document.getElementById(codeID).innerHTML = "<h1 style=\"color:white\">" + storeTitle + " <button onclick=\"document.getElementById('"+codeID+"').remove()\">x</button></h1><br><label style=\"color:white\">" + storeMessage + "</label>";
+        return "Dialog settings refreshed";
+      }else{
+        return "This dialog is already closed.";
+      }
     },
     setTitle: function(argtitle = "Announcement"){
-      storeTitle = title;
-      this.refreshDialog();
-      return "Dialog settings changed";
+      if (document.getElementById(codeID)){
+        storeTitle = argtitle;
+        this.refreshDialog();
+        return "Dialog settings changed";
+      }else{
+        return "This dialog is already closed.";
+      }
     },
     setMessage: function(argmessage = "Missing String"){
-      storeMessage = argmessage;
-      this.refreshDialog();
-      return "Dialog settings changed";
+      if (document.getElementById(codeID)){
+        storeMessage = argmessage;
+        this.refreshDialog();
+        return "Dialog settings changed";
+      }else{
+        return "This dialog is already closed.";
+      }
     }
   }
 }
